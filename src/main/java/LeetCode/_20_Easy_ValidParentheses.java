450396b58de3bd696e50a2be8ee8ebf7716c9197
Thilakeswar <thilak0411@rocketmail.com>
2023-03-31 12:57:34 +0530

    leetcode 21 solution

--- src/main/java/LeetCode/_20_Easy_ValidParentheses.java
+++ src/main/java/LeetCode/_20_Easy_ValidParentheses.java
@@ -4,42 +4,42 @@ import java.util.Stack;
 
 /**
  * https://leetcode.com/problems/valid-parentheses/
- * <p>
+ * 
  * 20. Valid Parentheses
  * Easy
  * 18.7K
  * 1.1K
  * Companies
- * <p>
+ * 
  * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
- * <p>
+ * 
  * An input string is valid if:
- * <p>
+ * 
  * Open brackets must be closed by the same type of brackets.
  * Open brackets must be closed in the correct order.
  * Every close bracket has a corresponding open bracket of the same type.
- * <p>
- * <p>
- * <p>
+ * 
+ * 
+ * 
  * Example 1:
- * <p>
+ * 
  * Input: s = "()"
  * Output: true
- * <p>
+ * 
  * Example 2:
- * <p>
+ * 
  * Input: s = "()[]{}"
  * Output: true
- * <p>
+ * 
  * Example 3:
- * <p>
+ * 
  * Input: s = "(]"
  * Output: false
- * <p>
- * <p>
- * <p>
+ * 
+ * 
+ * 
  * Constraints:
- * <p>
+ * 
  * 1 <= s.length <= 104
  * s consists of parentheses only '()[]{}'.
  */
